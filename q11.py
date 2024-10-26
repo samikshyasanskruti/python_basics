21a07d3e3754cdcdd3531805e590a67039a6a919
Samikshya <samikshyasanskrutiswain@gmail.com>
2024-10-26 17:32:11 +0530

    done

---
+++ q11.py
@@ -0,0 +1,4 @@
+widgets=float(input('enter the number of widgets:'))
+gizmos=float(input('enter the number of gizmos:'))
+tot_weight=(widgets*75)+(gizmos*112)
+print('the total weight is',tot_weight)
