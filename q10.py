f192858083652a652978d585ca34d2b1ae5c6389
Samikshya <samikshyasanskrutiswain@gmail.com>
2024-10-26 17:15:01 +0530

    done

---
+++ q10.py
@@ -0,0 +1,4 @@
+length=float(input('enter the length of the room in feet:'))
+width=float(input('enter the width of the room in feet:'))
+area=length*width
+print('The area of the room is',area, 'feet')
